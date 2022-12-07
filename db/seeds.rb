a61c6ff501a035807c5de38fc4e6ac9d86e558e6
Alex Lam <alam.seraph@gmail.com>
2022-12-06 18:10:31 -0800

    added 100 generated contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+100.times do 
+contact = Contact.new(first_name: Faker::Name.name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::Internet.email).save
+end 
